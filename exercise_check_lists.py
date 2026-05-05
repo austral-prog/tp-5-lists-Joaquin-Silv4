66e9be5c45d2d02415656f81a52df04663feb4df
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-05-05 11:12:08 -0300

    Update exercise_check_lists.py

--- exercise_check_lists.py
+++ exercise_check_lists.py
@@ -12,9 +12,9 @@ def check_lists(lista1, lista2):
     Returns:
         True si ambas listas tienen el mismo tercer elemento, False en caso contrario
     """
-    if lista1 == lista2:
-        return True
-    else:
+
+    if len(lista1) < 3 or len(lista2) < 3:
         return False
+    return lista1[2] == lista2[2]
 
 print(check_lists(['Black', 'Pink', 'Yellow', 'Red', 'Green', 'White'],['Red', 'Green', 'Yellow', 'Black', 'Pink']))
